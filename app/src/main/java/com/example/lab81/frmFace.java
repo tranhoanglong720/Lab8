3de9c4896b26e77efb12c3e11a9f28927c9ca1a5
Tran Hoang Long <tranhoanglong720@gmail.com>
2022-04-18 18:11:16 +0700

    Lab 8 UI

---
+++ app/src/main/java/com/example/lab81/frmFace.java
@@ -0,0 +1,14 @@
+package com.example.lab81;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frmFace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_face);
+    }
+}
