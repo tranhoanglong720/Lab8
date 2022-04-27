7800d5ec348a595cafa38cb9e8f161e8c3eb5b17
Tran Hoang Long <tranhoanglong720@gmail.com>
2022-04-27 20:01:43 +0700

    Add User Len RealTimeDataBase

--- app/src/main/java/com/example/lab81/User.java
+++ app/src/main/java/com/example/lab81/User.java
@@ -1,2 +1,84 @@
-package com.example.lab81;public class User {
+package com.example.lab81;
+
+public class User {
+    private String name;
+    private String email;
+    private String pass;
+    private String pass2;
+    public int normal;
+    public int happy;
+    public int unhappy;
+
+
+    public User(String name, String email, String pass, String pass2, int normal, int happy, int unhappy) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+        this.pass2 = pass2;
+        this.normal = normal;
+        this.happy = happy;
+        this.unhappy = unhappy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getPass2() {
+        return pass2;
+    }
+
+    public void setPass2(String pass2) {
+        this.pass2 = pass2;
+    }
+
+    public int getNormal() {
+        return normal;
+    }
+
+    public void setNormal(int normal) {
+        this.normal = normal;
+    }
+
+    public int getHappy() {
+        return happy;
+    }
+
+    public void setHappy(int happy) {
+        this.happy = happy;
+    }
+
+    public int getUnhappy() {
+        return unhappy;
+    }
+
+    public void setUnhappy(int unhappy) {
+        this.unhappy = unhappy;
+    }
+
+
+    public User(){
+
+    }
+
 }
--- app/src/main/java/com/example/lab81/frmDangKy.java
+++ app/src/main/java/com/example/lab81/frmDangKy.java
@@ -1,6 +1,7 @@
 package com.example.lab81;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,50 +17,64 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class frmDangKy extends AppCompatActivity {
-    private static final String TAG="LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
     Button btndn;
-    EditText edtemail,edtpass;
+    EditText edtemail,edtpass,edtname,edtpass2;
 
-    private FirebaseAuth mAuth;
+   // private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_dang_ky);
-        mAuth = FirebaseAuth.getInstance();
+      //  mAuth = FirebaseAuth.getInstance();
         Find();
         btndn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(edtemail.getText().length()==0)
-                {
-                    edtemail.setError("Please enter your email!");
-                }
-                else
-                if(edtpass.getText().length()==0)
-                {
-                    edtpass.setError("Please enter your password!");
-                }
-                else{
-                    SignIn();
-                }
+                String name=edtname.getText().toString().trim();
+                String email=edtemail.getText().toString().trim();
+                String pass=edtpass.getText().toString().trim();
+                String pass2=edtpass2.getText().toString().trim();
+                int normal=0;
+                int happy=0;
+                int unhappy=0;
+                User user=new User(name,email,pass,pass2,normal,happy,unhappy);
+               AddUser(user);
 
             }
         });
     }
-    private void Find(){
-        btndn=findViewById(R.id.btnDangNhap2);
-        edtemail=findViewById(R.id.editemail);
-        edtpass=findViewById(R.id.editpass);
+
+    private void AddUser(User user){
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference mRef=database.getReference("ListUser");
+        String  pathObject=String.valueOf(user.getEmail());
+        mRef.child(pathObject).setValue(user, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(frmDangKy.this, "DK thanh cong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-    private  void SignIn()
+    private void Find(){
+        btndn=findViewById(R.id.btnDangNhap3);
+        edtemail=findViewById(R.id.editemaildk);
+        edtpass=findViewById(R.id.editpassdk);
+        edtpass2=findViewById(R.id.editpass2dk);
+        edtname=findViewById(R.id.edittendk);
+    }
+   /* private  void SignIn()
     {
 
         final Intent intentface=new Intent(this,frmFace.class);
         String Email=edtemail.getText().toString();
         String Pass=edtpass.getText().toString();
+        String Name=edtname.getText().toString();
         mAuth.createUserWithEmailAndPassword(Email, Pass)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -78,5 +93,5 @@ public class frmDangKy extends AppCompatActivity {
                         }
                     }
                 });
-    }
+    }*/
 }
